b358334af796dbc0d8f44d19caac7d0107368d29
Yura Gudan <yura.gudan@gmail.com>
2019-05-30 20:29:49 +0300

    change setting.py

--- flightsrest/settings.py
+++ flightsrest/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# Deploy for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
